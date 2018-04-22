1afda4b42dbd13edbe3153215ca375acc5ce9569
Patrick M. Niedzielski <patrick@pniedzielski.net>
2018-04-22 15:49:39 +0000

    [plumbing] Refactor site generator
    This patch adds comments to make the structure of the site generator
    clearer.  In particular, this patch
    
      - adds a header comment, with a list of sections in the file
    
      - adds comments delineating file sections,
    
      - splits out the static resource behavior rules, and
    
      - adds documentation comments to free functions.
    
    Because future patches are going to introduce more complex behavior,
    this is a good time to do this.  This patch does not change any site
    generation funcitonality.
    
    License: AGPLv3+
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- pniedzielski-net.hs
+++ pniedzielski-net.hs
@@ -1,16 +1,48 @@
 -------------------------------------------------------------------------------
+--                      PNIEDZIELSKI.NET SITE GENERATOR                      --
+--                      -------------------------------                      --
+--                                                                           --
+-- Copyright © 2018, Patrick M. Niedzielski <patrick@pniedzielski.net>.      --
+-- See LICENSE file for copying information.                                 --
+--                                                                           --
+-- TABLE OF CONTENTS:                                                        --
+--     1. Compile templates                                                  --
+--     2. Home page                                                          --
+--     3. Articles                                                           --
+--     4. Static resources                                                   --
+--                                                                           --
+-- APPENDICES:                                                               --
+--     A. Behaviors                                                          --
+--     B. Compliers                                                          --
+--                                                                           --
+-------------------------------------------------------------------------------
+
+
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UnicodeSyntax     #-}
+
 import    Hakyll
 import    Prelude.Unicode
 import    Control.Monad.Unicode
 import    Data.Monoid.Unicode
 import    Data.List
--------------------------------------------------------------------------------
 
 main ∷ IO ()
 main = hakyll $ do
+
+
+-------------------------------------------------------------------------------
+--                                                      1. COMPILE TEMPLATES --
+-------------------------------------------------------------------------------
+
+
   match "templates/*" $ compile templateBodyCompiler
+
+
+-------------------------------------------------------------------------------
+--                                                              2. HOME PAGE --
+-------------------------------------------------------------------------------
+
 
   match "index.html" $ do
     route idRoute
@@ -19,29 +51,43 @@ main = hakyll $ do
       ≫= loadAndApplyTemplate "templates/default.html"
            (constField "url" "/" ⊕ defaultContext)
 
-  match cachables $ do
-    route idRoute
-    compile copyFileCompiler
-
-  match articles $ do
-    route idRoute
-    compile copyFileCompiler
-
-  match res $ do
-    route idRoute
-    compile copyFileCompiler
-
-      where cachables = fromList [ "serviceworker.js"
-                                 , "pniedzielski.asc"
-                                 , "feed.xml" ]
-            articles  = "2016/**/*.html" .||.
-                        "2017/**/*.html" .||.
-                        "2018/**/*.html"
-            res       = fromList [ "css/styles.css"
-                                 , "images/favicon.ico"
-                                 , "me.jpg" ]
 
 -------------------------------------------------------------------------------
+--                                                               3. ARTICLES --
+-------------------------------------------------------------------------------
+
+
+  match ("2016/**/*.html" .||. "2017/**/*.html" .||. "2018/**/*.html")
+    staticBehavior
+
+
+-------------------------------------------------------------------------------
+--                                                       4. STATIC RESOURCES --
+-------------------------------------------------------------------------------
+
+
+  match (fromList [ "css/styles.css"   , "images/favicon.ico"
+                  , "me.jpg"           , "serviceworker.js"
+                  , "pniedzielski.asc" , "feed.xml"           ])
+    staticBehavior
+
+
+-------------------------------------------------------------------------------
+--                                                              A. BEHAVIORS --
+-------------------------------------------------------------------------------
+
+
+-- Copy a file without modifying it.
+staticBehavior ∷ Rules ()
+staticBehavior = do
+  route   idRoute
+  compile copyFileCompiler
+
+
+-------------------------------------------------------------------------------
+--                                                              B. COMPILERS --
+-------------------------------------------------------------------------------
+
 
 -- Indent the body of a resource by 𝑛 spaces.
 indentBodyBy ∷ Int → Item String → Compiler (Item String)
